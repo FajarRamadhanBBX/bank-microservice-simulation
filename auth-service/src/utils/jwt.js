e7054f5bd6508d690b214f18d88192e5bf8c0134
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2026-01-25 16:59:06 +0700

    update(auth): add role to convert to jwt

--- auth-service/src/utils/jwt.js
+++ auth-service/src/utils/jwt.js
@@ -3,7 +3,7 @@ const { jwtSecret, jwtExpiresIn } = require('../config/jwt');
 
 const generateToken = (user) => {
     const token = jwt.sign(
-        {id: user.id},
+        {id: user.id, role: user.role},
         jwtSecret,
         { expiresIn: jwtExpiresIn }
     )
