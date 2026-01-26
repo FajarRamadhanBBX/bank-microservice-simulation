59228ccb351a5de490c4ceb412a67d613d3310da
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2026-01-26 11:50:07 +0700

    update(user): change userService function name

--- user-service/src/controllers/user.controller.js
+++ user-service/src/controllers/user.controller.js
@@ -32,7 +32,7 @@ const getAllProfiles = async (req, res) => {
 const getMe = async (req, res) => {
     try {
         const userId = req.headers['x-user-id'];
-        const user = await userService.fetchUserById(userId);
+        const user = await userService.fetchProfileByAuthId(userId);
         res.status(200).json({ user });
     } catch (err) {
         res.status(404).json({ error: err.message });
