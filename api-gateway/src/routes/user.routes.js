7191a2a8172fec7e65e87da723bd8a9cd3e87cb4
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2026-01-27 07:24:34 +0700

    feat(user): add route code

--- api-gateway/src/routes/user.routes.js
+++ api-gateway/src/routes/user.routes.js
@@ -2,13 +2,48 @@ const router = require("express").Router();
 const authMiddleware = require("../middlewares/auth.middleware");
 const allowRolesMiddleware = require("../middlewares/role.middleware");
 const {
-    getMe
+    createProfile,
+    getProfile,
+    getAllProfiles,
+    getMyProfile,
+    updateMyProfile,
+    deleteMyProfile
 } = require("../proxies/user.proxy");
+
+router.post("/profiles",
+    authMiddleware,
+    allowRolesMiddleware(["user", "admin"]),
+    createProfile
+)
+
+router.get("/profiles",
+    authMiddleware,
+    allowRolesMiddleware(["admin"]),
+    getProfile
+)
+
+router.post("/admin/profiles",
+    authMiddleware,
+    allowRolesMiddleware(["admin"]),
+    getAllProfiles
+)
 
 router.get("/me",
     authMiddleware,
     allowRolesMiddleware(["user", "admin"]),
-    getMe
+    getMyProfile
 );
+
+router.put("/me",
+    authMiddleware,
+    allowRolesMiddleware(["user", "admin"]),
+    updateMyProfile
+)
+
+router.delete("/me",
+    authMiddleware,
+    allowRolesMiddleware(["user", "admin"]),
+    deleteMyProfile
+)
 
 module.exports = router;
