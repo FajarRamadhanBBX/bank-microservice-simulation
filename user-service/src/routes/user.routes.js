90c823fe14b8dd9513f03cdcc9bff1d8fdf38aad
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2026-01-28 00:41:08 +0700

    update(user): change endpoint path

--- user-service/src/routes/user.routes.js
+++ user-service/src/routes/user.routes.js
@@ -2,10 +2,8 @@ const router = require('express').Router();
 const userController = require('../controllers/user.controller');
 
 router.post('/profiles', userController.createProfile);
-router.get('/profiles/:auth_id', userController.getProfile);
-router.get('/profiles', userController.getAllProfiles);
-router.get('/me', userController.getMe);
-router.put('/me', userController.updateMyProfile);
-router.delete('/me', userController.deleteMyProfile);
+router.get('/me/profiles', userController.getProfile);
+router.put('/me/profiles', userController.updateMyProfile);
+router.get('/admin/profiles', userController.getAllProfiles);
 
 module.exports = router;
