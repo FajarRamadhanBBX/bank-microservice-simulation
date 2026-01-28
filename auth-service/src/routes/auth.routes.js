4656170d270e8b46d5de7e6f1036647f8183421b
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2026-01-28 19:37:48 +0700

    update(auth): update path

--- auth-service/src/routes/auth.routes.js
+++ auth-service/src/routes/auth.routes.js
@@ -6,8 +6,9 @@ router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.put("/update-password", authController.updatePassword);
 router.put("/me/deactivate", authController.deactivateAuth);
+router.post("/register/admin", authController.registerAdmin);
 router.get("/admin/users", authController.getAllUsers);
-router.get("/admin/users/email/:email", authController.getUser);
-router.put("/admin/users/:id/status", authController.changeStatusByAdmin);
+router.get("/admin/users/email", authController.getUser);
+router.put("/admin/users/status", authController.changeStatusByAdmin);
 
 module.exports = router;
