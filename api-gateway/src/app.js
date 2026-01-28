b1ad27176757aaa41de85025e2d344e4d37ca2a8
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2026-01-28 11:06:02 +0700

    update(gw): add auth routes to app

--- api-gateway/src/app.js
+++ api-gateway/src/app.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const userRoutes = require('./routes/user.routes');
+const authRoutes = require("./routes/auth.routes")
+const userRoutes = require("./routes/user.routes");
 
 app.use(express.json());
-app.use('/users', userRoutes);
+app.use("/auth", authRoutes)
+app.use("/users", userRoutes);
 
 module.exports = app;
