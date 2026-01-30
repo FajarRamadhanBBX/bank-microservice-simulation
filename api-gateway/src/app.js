d9ab090483366bec53ef9cb01f8aca7bf5b49425
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2026-01-31 02:48:37 +0700

    feat(gw): trans app

--- api-gateway/src/app.js
+++ api-gateway/src/app.js
@@ -3,10 +3,12 @@ const app = express();
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
 const accountRoutes = require("./routes/account.routes");
+const transactionRoutes = require("./routes/transaction.routes");
 
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/accounts", accountRoutes);
+app.use("/transactions", transactionRoutes);
 
 module.exports = app;
