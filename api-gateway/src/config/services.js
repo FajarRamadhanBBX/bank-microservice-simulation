c5c7495f52b645cba63ff30575922d2b9a8e23c7
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2026-01-31 14:29:24 +0700

    update(gw):: change localhost to service name

--- api-gateway/src/config/services.js
+++ api-gateway/src/config/services.js
@@ -1,6 +1,6 @@
 module.exports = {
-    AUTH_SERVICE: "http://localhost:4001",
-    USER_SERVICE: "http://localhost:4002",
-    ACCOUNT_SERVICE: "http://localhost:4003",
-    TRANSACTION_SERVICE: "http://localhost:4004"
+    AUTH_SERVICE: "http://auth-service:4001",
+    USER_SERVICE: "http://user-service:4002",
+    ACCOUNT_SERVICE: "http://account-service:4003",
+    TRANSACTION_SERVICE: "http://transaction--service:4004"
 };
