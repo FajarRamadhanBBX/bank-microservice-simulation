60f2502ceca55f01383c17500a21691fc5f6fa73
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2026-01-31 19:28:31 +0700

    feat(root): add deploy.sh for CI/CD

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Define variabel in github secrets, call in github workflos
+PROJECT_DIR=$1
+
+if [ -z "$PROJECT_DIR" ]; then
+  echo "Error: Path project belum didefinisikan!"
+  exit 1
+fi
+
+echo "Deploying to: $PROJECT_DIR"
+cd "$PROJECT_DIR" || { echo "Directory not found"; exit 1; }
+
+echo "Update configuration..."
+git pull origin main
+
+echo "Pulling latest images..."
+docker-compose pull
+
+echo "Restart containers..."
+docker-compose up -d
+
+echo "Cleaning"
+docker image prune -f
+
+echo "Deployment Successfully!"
