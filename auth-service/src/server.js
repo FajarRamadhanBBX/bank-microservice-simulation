f20517d39e1df8fe25ee2caad84c2e5adb5141b5
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2026-01-31 16:18:57 +0700

    update(all): add initDb at server

--- auth-service/src/server.js
+++ auth-service/src/server.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
 const app = require("./app");
+const initDb = require("./utils/initDb");
 
 const PORT = process.env.APP_PORT;
+const startServer = async () => {
+    await initDb();
 
-app.listen(PORT, () => {
-    console.log("Auth service runnin on port", PORT)
-})
+    app.listen(PORT, () => {
+        console.log(`Auth service running on port ${PORT}`);
+    });
+};
+
+startServer();
