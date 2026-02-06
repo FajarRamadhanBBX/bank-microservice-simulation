00205b6dcc149675712e991c0ba8746bff9c9736
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2026-02-07 00:24:23 +0700

    Revert "update(root): change run container port"
    This reverts commit 22aa1d0f497ddd0f773e6eee6d7e1c01e5e19a0d.

--- scripts/smoke-test-auth.sh
+++ scripts/smoke-test-auth.sh
@@ -4,47 +4,27 @@ IMAGE_NAME=auth-service-test:test
 CONTAINER_NAME=auth-service-test
 PORT_HOST=5001
 PORT_APP=4001
-ENDPOINT="http://localhost:${PORT_HOST}/auth/health"
+ENDPOINT="http://localhost:${PORT_HOST}/health"
 
-# Setup koneksi ke Database Host (sesuai docker ps kamu sebelumnya)
-# Auth DB kamu jalan di port 5431 di host
-DB_HOST="host.docker.internal"
-DB_PORT=5431
-DB_USER="auth_user"     # Sesuaikan dengan .env kamu
-DB_PASS="authpass"     # Sesuaikan dengan .env kamu
-DB_NAME="auth_db"      # Sesuaikan dengan .env kamu
-
-MAX_RETRIES=60 # 200 kebanyakan, 30 cukup
+MAX_RETRIES=200
 DELAY=2
 
+# Stop if error exist
 set -e
 
-echo "build image ..."
+echo "buid image ..."
 docker build -t $IMAGE_NAME ./services/auth-service
 
 echo "remove container with same name ..."
 docker rm -f $CONTAINER_NAME 2>/dev/null || true
 
 echo "run container ..."
-# PERUBAHAN PENTING ADA DISINI:
-docker run -d \
-  -p $PORT_HOST:$PORT_APP \
-  --add-host=host.docker.internal:host-gateway \
-  -e APP_PORT=$PORT_APP \
-  -e DB_HOST=$DB_HOST \
-  -e DB_PORT=$DB_PORT \
-  -e DB_USER=$DB_USER \
-  -e DB_PASSWORD=$DB_PASS \
-  -e DB_NAME=$DB_NAME \
-  --name $CONTAINER_NAME \
-  $IMAGE_NAME
+docker run -d -p $PORT_HOST:$PORT_APP --name $CONTAINER_NAME $IMAGE_NAME
 
 set +e
 
-echo "Waiting for service to start..."
 count=0
 while [ $count -lt $MAX_RETRIES ]; do
-    # Tambahkan -v biar kelihatan kalau ada error http
     if curl -sf $ENDPOINT > /dev/null; then
         echo "Service is UP!"
         break
@@ -57,7 +37,7 @@ done
 
 if [ $count -eq $MAX_RETRIES ]; then
     echo "Timeout! Service failed to start"
-    echo "Container logs:"
+    echo "Container logs"
     docker logs $CONTAINER_NAME
     echo "--------------------------------------"
     docker rm -f $CONTAINER_NAME
