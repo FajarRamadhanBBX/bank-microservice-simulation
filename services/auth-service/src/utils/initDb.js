f71c6857f3188d63a12d2a402721e6337dca9c98
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2026-02-07 00:20:46 +0700

    Revert "update(auth): add checker"
    This reverts commit fdedcc1cc581912452c80ab881e1bd17129f8531.

--- services/auth-service/src/utils/initDb.js
+++ services/auth-service/src/utils/initDb.js
@@ -3,16 +3,8 @@ const db = require("../config/db");
 const initDb = async () => {
     try {
         await db.query(`
-            -- 1. Cek dulu apakah Type sudah ada, jika belum baru buat (Pakai blok DO)
-            DO $$
-            BEGIN
-                IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'auth_role') THEN
-                    CREATE TYPE auth_role AS ENUM ('admin', 'user');
-                END IF;
-            END$$;
-
-            -- 2. Buat tabel HANYA jika belum ada (IF NOT EXISTS)
-            CREATE TABLE IF NOT EXISTS users (
+            CREATE TYPE auth_role AS ENUM ('admin', 'user');
+            CREATE TABLE users (
                 id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
                 email VARCHAR(255) UNIQUE NOT NULL,
                 password TEXT NOT NULL,
@@ -22,9 +14,8 @@ const initDb = async () => {
                 updated_at TIMESTAMP DEFAULT NOW()
             );
         `);
-        console.log("Database initialized (Type & Table checked).");
+        console.log("Table 'auth' created, or already exists.");
     } catch (error) {
-        // Log error tetap penting, tapi sekarang error "already exists" tidak akan muncul lagi
         console.error("Error creating table:", error.message);
     }
 };
