ac5c5dcecbae11d0514681fb2c1791728cf5b572
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2026-02-07 11:34:34 +0700

    update(test): add network connection to docker compoese

--- scripts/smoke-test-auth.sh
+++ scripts/smoke-test-auth.sh
@@ -1,49 +1,70 @@
 #!/bin/bash
-
-IMAGE_NAME=auth-service-test:test
-CONTAINER_NAME=auth-service-test
-PORT_HOST=5001
-PORT_APP=4001
-ENDPOINT="http://localhost:${PORT_HOST}/health"
-
-MAX_RETRIES=200
-DELAY=2
-
-# Stop if error exist
 set -e
 
-echo "buid image ..."
-docker build -t $IMAGE_NAME ./services/auth-service
+# Test Configuration
+IMAGE_NAME="auth-service-test:test"
+CONTAINER_NAME="auth-service-test"
+APP_PORT=4001
+PORT_HOST=5001
+ENDPOINT="http://localhost:${PORT_HOST}/auth/health"
 
-echo "remove container with same name ..."
+# Adjust with your network
+DOCKER_NETWORK=$DOCKER_NETWORK
+
+# DB Configuration
+DB_HOST="auth-postgres" # from service name in compose
+DB_PORT=5432
+DB_USER=$AUTH_USER
+DB_PASS=$AUTH_PASSWORD
+DB_NAME=$AUTH_DB
+
+MAX_RETRIES=30
+DELAY=2
+
+# Clean old container
+echo "Remove old container (if exists)..."
 docker rm -f $CONTAINER_NAME 2>/dev/null || true
 
-echo "run container ..."
-docker run -d -p $PORT_HOST:$PORT_APP --name $CONTAINER_NAME $IMAGE_NAME
+# Build image test
+echo "Build auth-service image..."
+docker build -t $IMAGE_NAME ../services/auth-service
 
-set +e
+# Run container (join with compose network)
+echo "Run smoke test auth service container..."
+docker run -d \
+  --name $CONTAINER_NAME \
+  --network $DOCKER_NETWORK \
+  -p $PORT_HOST:$APP_PORT \
+  -e APP_PORT=$APP_PORT \
+  -e DB_HOST=$DB_HOST \
+  -e DB_PORT=$DB_PORT \
+  -e DB_USER=$DB_USER \
+  -e DB_PASSWORD=$DB_PASS \
+  -e DB_NAME=$DB_NAME \
+  $IMAGE_NAME
 
+echo "Waiting for service to be healthy..."
 count=0
-while [ $count -lt $MAX_RETRIES ]; do
-    if curl -sf $ENDPOINT > /dev/null; then
-        echo "Service is UP!"
-        break
-    fi
+until curl -sf $ENDPOINT > /dev/null; do
+  sleep $DELAY
+  count=$((count + 1))
+  echo "Attempt $count/$MAX_RETRIES"
 
-    sleep $DELAY
-    count=$((count+1))
-    echo "Service not ready, hit counting: ${count}"
+  if [ $count -ge $MAX_RETRIES ]; then
+    echo "Smoke test FAILED"
+    echo "=========== Container logs: ==========="
+    docker logs $CONTAINER_NAME
+    docker rm -f $CONTAINER_NAME
+    echo "======================================="
+    exit 1
+  fi
 done
 
-if [ $count -eq $MAX_RETRIES ]; then
-    echo "Timeout! Service failed to start"
-    echo "Container logs"
-    docker logs $CONTAINER_NAME
-    echo "--------------------------------------"
-    docker rm -f $CONTAINER_NAME
-    exit 1
-fi
-
-echo "✅ Smoke test Passed for auth-service"
+echo "Smoke test PASSED"
+# Clean if passed
+echo "Container logs:"
+echo "=========== Container logs: ==========="
+docker logs $CONTAINER_NAME
 docker rm -f $CONTAINER_NAME
+echo "======================================="
 exit 0
--- scripts/smoke-test-user.sh
+++ scripts/smoke-test-user.sh
@@ -1,48 +1,70 @@
 #!/bin/bash
+set -e
 
-IMAGE_NAME=user-service-test:test
-CONTAINER_NAME=user-service-test
-PORT=5002
-ENDPOINT="http://localhost:${PORT}/health"
+# Test Configuration
+IMAGE_NAME="user-service-test:test"
+CONTAINER_NAME="user-service-test"
+APP_PORT=4002
+PORT_HOST=5002
+ENDPOINT="http://localhost:${PORT_HOST}/users/health"
+
+# Adjust with your network
+DOCKER_NETWORK=$DOCKER_NETWORK
+
+# DB Configuration
+DB_HOST="user-postgres" # from service name in compose
+DB_PORT=5432
+DB_USER=$USERS_USER
+DB_PASS=$USERS_PASSWORD
+DB_NAME=$USERS_DB
 
 MAX_RETRIES=30
 DELAY=2
 
-# Stop if error exist
-set -e
-
-echo "buid image ..."
-docker build -t $IMAGE_NAME ./services/user-service
-
-echo "remove container with same name ..."
+# Clean old container
+echo "Remove old container (if exists)..."
 docker rm -f $CONTAINER_NAME 2>/dev/null || true
 
-echo "run container ..."
-docker run -d -p $PORT:$PORT --name $CONTAINER_NAME $IMAGE_NAME
+# Build image test
+echo "Build user-service image..."
+docker build -t $IMAGE_NAME ../services/user-service
 
-set +e
+# Run container (join with compose network)
+echo "Run smoke test user service container..."
+docker run -d \
+  --name $CONTAINER_NAME \
+  --network $DOCKER_NETWORK \
+  -p $PORT_HOST:$APP_PORT \
+  -e APP_PORT=$APP_PORT \
+  -e DB_HOST=$DB_HOST \
+  -e DB_PORT=$DB_PORT \
+  -e DB_USER=$DB_USER \
+  -e DB_PASSWORD=$DB_PASS \
+  -e DB_NAME=$DB_NAME \
+  $IMAGE_NAME
 
+echo "Waiting for service to be healthy..."
 count=0
-while [ $count -lt $MAX_RETRIES ]; do
-    if curl -sf $ENDPOINT > /dev/null; then
-        echo "Service is UP!"
-        break
-    fi
+until curl -sf $ENDPOINT > /dev/null; do
+  sleep $DELAY
+  count=$((count + 1))
+  echo "Attempt $count/$MAX_RETRIES"
 
-    sleep $DELAY
-    count=$((count+1))
-    echo "Service not ready, hit counting: ${count}"
+  if [ $count -ge $MAX_RETRIES ]; then
+    echo "Smoke test FAILED"
+    echo "=========== Container logs: ==========="
+    docker logs $CONTAINER_NAME
+    docker rm -f $CONTAINER_NAME
+    echo "======================================="
+    exit 1
+  fi
 done
 
-if [ $count -eq $MAX_RETRIES ]; then
-    echo "Timeout! Service failed to start"
-    echo "Container logs"
-    docker logs $CONTAINER_NAME
-    echo "--------------------------------------"
-    docker rm -f $CONTAINER_NAME
-    exit 1
-fi
-
-echo "✅ Smoke test Passed for user-service"
+echo "Smoke test PASSED"
+# Clean if passed
+echo "Container logs:"
+echo "=========== Container logs: ==========="
+docker logs $CONTAINER_NAME
 docker rm -f $CONTAINER_NAME
+echo "======================================="
 exit 0
